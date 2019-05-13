d86949a20b6d254d4e88c571505ad4c4d4d4d620
Manel2313 <djemal.manel@gmail.com>
2019-05-13 14:54:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      value: ''
+    }
+  }
+  timer = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+    console.log(e.target.value)
+  }
+  msToTime = (props) => {
+    var seconds = parseInt((props / 1000) % 60)
+      , minutes = parseInt((props / (1000 * 60)) % 60)
+      , hours = parseInt((props / (1000 * 60 * 60)) % 24);
+      
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return hours + " : " + minutes + " : " + seconds;
+  }
+  render() {
+    let x = this.state.value
+    return (
+      <div className="App">
+        <h1>Merci de saisir le nombre à convertir !</h1>
+        <input type="text" name="fname"
+          value={this.state.value}
+          onChange={this.timer} />
+        <div className="result">
+          <p className="resultat">{this.msToTime(x)}</p>
+          <div className="description">
+            <p>Hour</p>
+            <p>Minute</p>
+            <p>Second</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
